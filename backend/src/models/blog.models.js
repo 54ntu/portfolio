4494469bd02641c7c947a40f58dd5edc9c0ad49c
54ntu <chaudharysantaram8@gmail.com>
2024-06-15 22:07:00 +0545

    author field is added into the blog models

--- backend/src/models/blog.models.js
+++ backend/src/models/blog.models.js
@@ -1,22 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required:true
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    blog_img:{
-        type:String,   //url will be obtained from the cloudinary
-        required:true
+    blog_img: {
+      type: String, //url will be obtained from the cloudinary
+      required: true,
     },
-    description:{
-        type:String,
-        required:true
-    }
+    description: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Admin",
+    },
+  },
+  { timestamps: true }
+);
 
-},{timestamps:true});
-
-
-
-const Blog = mongoose.model('Blog',blogSchema);
-module.exports ={Blog};
+const Blog = mongoose.model("Blog", blogSchema);
+module.exports = { Blog };
