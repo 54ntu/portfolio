e4d4d5fd02fd6ffeafbe023dc18fdecf2fdb2719
54ntu <chaudharysantaram8@gmail.com>
2024-06-17 01:03:47 +0545

    admin page is getting ready

---
+++ frontend/src/Admin/AdminHomepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminHomepage = () => {
+  return (
+    <div>
+        <h1>home page data</h1>
+    </div>
+  )
+}
+
+export default AdminHomepage
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,8 @@ import './App.css'
 
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
-import AdminPanel from './AdminComponent/AdminPanel';
 import Appcomponent from './Appcomponent1';
+import AdminDashboard from './Admin/AdminDashboard';
 
 function App() {
 
@@ -12,7 +12,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={ <Appcomponent/> }/>
-        <Route path='/admin' element={ <AdminPanel/> }/>
+        <Route path='/admin' element={ <AdminDashboard/> }/>
        
       </Routes>
     </Router>
